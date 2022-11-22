08adaab1e717cb2eb2145a642a21408faa821d7e
Daniel Costa <danielrezcosta97@gmail.com>
2022-11-22 20:29:19 -0300

    Created API

---
+++ app/src/main/java/com/example/boredapplication/data/datasources/BoredApi.kt
@@ -0,0 +1,9 @@
+package com.example.boredapplication.data.datasources
+
+import com.example.boredapplication.data.models.BoredActivity
+import retrofit2.http.GET
+
+interface BoredApi {
+    @GET("/api/activity/")
+    fun getRandomEvent(): BoredActivity
+}
--- app/src/main/java/com/example/boredapplication/di/NetworkModule.kt
+++ app/src/main/java/com/example/boredapplication/di/NetworkModule.kt
@@ -1,5 +1,6 @@
 package com.example.boredapplication.di
 
+import com.example.boredapplication.data.datasources.BoredApi
 import okhttp3.OkHttpClient
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -7,14 +8,18 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 val networkModule = module {
     factory { provideOkHttpClient() }
-    single { provideRetrofit(get()) }
+    factory { provideRetrofit(get()) }
 }
 
 fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
-     Retrofit.Builder()
-        .baseUrl("http://www.boredapi.com")
+    Retrofit.Builder()
+        .baseUrl(BASE_URL)
         .client(okHttpClient)
         .addConverterFactory(MoshiConverterFactory.create())
         .build()
 
 fun provideOkHttpClient(): OkHttpClient = OkHttpClient().newBuilder().build()
+
+fun boredApi(retrofit: Retrofit): BoredApi = retrofit.create(BoredApi::class.java)
+
+private const val BASE_URL = "http://www.boredapi.com"
