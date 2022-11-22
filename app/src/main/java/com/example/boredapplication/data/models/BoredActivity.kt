38c03a5e0d99062ad57ad682d025335c9a60f675
Daniel Costa <danielrezcosta97@gmail.com>
2022-11-22 20:28:32 -0300

    Created model for activity

---
+++ app/src/main/java/com/example/boredapplication/data/models/BoredActivity.kt
@@ -0,0 +1,20 @@
+package com.example.boredapplication.data.models
+
+import com.squareup.moshi.Json
+
+data class BoredActivity(
+    @Json(name = "accessibility")
+    val accessibility: Double,
+    @Json(name = "activity")
+    val activity: String,
+    @Json(name = "key")
+    val key: String,
+    @Json(name = "link")
+    val link: String,
+    @Json(name = "participants")
+    val participants: Int,
+    @Json(name = "price")
+    val price: Double,
+    @Json(name = "type")
+    val type: String
+)
