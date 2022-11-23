71c472d594c247e940ad6acf678d76f8d35f10ea
Daniel Costa <danielrezcosta97@gmail.com>
2022-11-22 21:20:47 -0300

    Changed DTO object name

--- app/src/main/java/com/example/boredapplication/data/datasources/BoredApi.kt
+++ app/src/main/java/com/example/boredapplication/data/datasources/BoredApi.kt
@@ -1,10 +1,10 @@
 package com.example.boredapplication.data.datasources
 
-import com.example.boredapplication.data.models.BoredActivity
+import com.example.boredapplication.data.models.BoredActivityDTO
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface BoredApi {
     @GET("/api/activity/")
-    suspend fun getRandomEvent(@Query("type") type: String?): BoredActivity
+    suspend fun getRandomEvent(@Query("type") type: String?): BoredActivityDTO
 }
