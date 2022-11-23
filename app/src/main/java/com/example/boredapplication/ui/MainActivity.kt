16ef3f44d5437ce5770d4ee38378f75feddafdfd
Daniel Costa <danielrezcosta97@gmail.com>
2022-11-23 19:40:46 -0300

    Removed test logic from activity

--- app/src/main/java/com/example/boredapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/boredapplication/ui/MainActivity.kt
@@ -1,22 +1,12 @@
 package com.example.boredapplication.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.lifecycleScope
+import androidx.appcompat.app.AppCompatActivity
 import com.example.boredapplication.R
-import com.example.boredapplication.data.repositories.BoredRepository
-import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
-    private val teste by inject<BoredRepository>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-       lifecycleScope.launchWhenStarted {
-           teste.getRandomActivity(null)
-           println()
-       }
     }
 }
