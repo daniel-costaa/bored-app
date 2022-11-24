14bee4597fa2da82018f380a03bac36c51e227a4
Daniel Costa <danielrezcosta97@gmail.com>
2022-11-23 21:22:22 -0300

    Created queries to save and update entity

--- app/src/main/java/com/example/boredapplication/data/dao/BoredDao.kt
+++ app/src/main/java/com/example/boredapplication/data/dao/BoredDao.kt
@@ -1,11 +1,17 @@
 package com.example.boredapplication.data.dao
 
 import androidx.room.Dao
-import androidx.room.Query
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.IGNORE
+import androidx.room.Update
 import com.example.boredapplication.data.entities.BoredActivityEntity
+import com.example.boredapplication.data.entities.BoredActivityExtraData
 
 @Dao
 interface BoredDao {
-    @Query("SELECT * FROM activity_table")
-    fun getAll(): List<BoredActivityEntity>
+    @Insert(onConflict = IGNORE)
+    suspend fun saveActivity(activityEntity: BoredActivityEntity)
+
+    @Update(entity = BoredActivityEntity::class)
+    suspend fun updateExtraActivityDate(extraData: BoredActivityExtraData)
 }
