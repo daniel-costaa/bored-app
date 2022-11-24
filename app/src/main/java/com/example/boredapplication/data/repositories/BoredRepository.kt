a12748489820c4a03635716326a2a18071d7a396
Daniel Costa <danielrezcosta97@gmail.com>
2022-11-23 21:22:42 -0300

    Created functions to save and update activities in repository

--- app/src/main/java/com/example/boredapplication/data/repositories/BoredRepository.kt
+++ app/src/main/java/com/example/boredapplication/data/repositories/BoredRepository.kt
@@ -1,10 +1,28 @@
 package com.example.boredapplication.data.repositories
 
+import com.example.boredapplication.data.dao.BoredDao
 import com.example.boredapplication.data.datasources.BoredApi
+import com.example.boredapplication.data.entities.BoredActivityExtraData
+import com.example.boredapplication.data.models.ActivityStatus
 import com.example.boredapplication.data.models.ActivityTypes
+import com.example.boredapplication.data.models.BoredActivityUi
+import com.example.boredapplication.utils.toEntityActivity
 import com.example.boredapplication.utils.toUiActivity
 
-class BoredRepository(private val remoteDataSource: BoredApi) {
+class BoredRepository(
+    private val remoteDataSource: BoredApi,
+    private val localDataSource: BoredDao
+) {
     suspend fun getRandomActivity(type: ActivityTypes?) =
-        remoteDataSource.getRandomEvent(type?.name?.lowercase()).toUiActivity()
+        remoteDataSource
+            .getRandomEvent(type?.name?.lowercase())
+            .toUiActivity()
+
+    suspend fun saveActivity(uiActivity: BoredActivityUi) {
+        localDataSource.saveActivity(uiActivity.toEntityActivity())
+    }
+
+    suspend fun updateExtraData(key: String, status: ActivityStatus, timeSpent: Long) {
+        localDataSource.updateExtraActivityDate(BoredActivityExtraData(key, status, timeSpent))
+    }
 }
