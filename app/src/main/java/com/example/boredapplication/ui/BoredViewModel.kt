1c34d2c2567abde95c5330f3ce3390356e756d73
Daniel Costa <danielrezcosta97@gmail.com>
2022-11-23 21:23:21 -0300

    Added logic in view model to save data

--- app/src/main/java/com/example/boredapplication/ui/BoredViewModel.kt
+++ app/src/main/java/com/example/boredapplication/ui/BoredViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.boredapplication.data.models.ActivityStatus
 import com.example.boredapplication.data.models.ActivityTypes
 import com.example.boredapplication.data.models.BoredActivityUi
 import com.example.boredapplication.data.repositories.BoredRepository
@@ -13,10 +14,33 @@ class BoredViewModel(private val boredRepository: BoredRepository) : ViewModel()
     private val _currentActivity = MutableLiveData<BoredActivityUi>()
     val currentActivity: LiveData<BoredActivityUi>; get() = _currentActivity
 
+    var startTime: Long = 0L
+
     fun getActivity(type: ActivityTypes?) {
         viewModelScope.launch {
             val activity = boredRepository.getRandomActivity(type)
             _currentActivity.postValue(activity)
         }
+    }
+
+    fun saveActivity() {
+        viewModelScope.launch {
+            currentActivity.value?.let {
+                boredRepository.saveActivity(it)
+            }
+        }
+    }
+
+    fun updateExtraData(status: ActivityStatus) {
+        viewModelScope.launch {
+            val timeSpent = System.currentTimeMillis() - startTime
+            currentActivity.value?.let {
+                boredRepository.updateExtraData(it.key, status, timeSpent)
+            }
+        }
+    }
+
+    fun setStartTime() {
+        startTime = System.currentTimeMillis()
     }
 }
