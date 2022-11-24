8fe022ec98bd90ee055e6a2f7a5c5757a533f33a
Daniel Costa <danielrezcosta97@gmail.com>
2022-11-23 21:23:03 -0300

    Added more fields to ui activity

--- app/src/main/java/com/example/boredapplication/data/models/BoredActivityUi.kt
+++ app/src/main/java/com/example/boredapplication/data/models/BoredActivityUi.kt
@@ -1,9 +1,13 @@
 package com.example.boredapplication.data.models
 
 data class BoredActivityUi(
+    val key: String,
     val accessibility: Double,
     val activity: String,
     val participants: Int,
     val price: Double,
-    val type: String
+    val type: String,
+    val status: ActivityStatus?,
+    val timeSpent: Long?,
+    val link: String = ""
 )
