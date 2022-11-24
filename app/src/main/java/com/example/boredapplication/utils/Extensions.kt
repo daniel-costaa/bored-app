4ac81ef9093b84956acdab5c6e959f94de320abf
Daniel Costa <danielrezcosta97@gmail.com>
2022-11-23 21:23:31 -0300

    Improved object mapping

--- app/src/main/java/com/example/boredapplication/utils/Extensions.kt
+++ app/src/main/java/com/example/boredapplication/utils/Extensions.kt
@@ -1,13 +1,31 @@
 package com.example.boredapplication.utils
 
+import com.example.boredapplication.data.entities.BoredActivityEntity
+import com.example.boredapplication.data.models.ActivityStatus
 import com.example.boredapplication.data.models.BoredActivityDTO
 import com.example.boredapplication.data.models.BoredActivityUi
 
 fun BoredActivityDTO.toUiActivity() = BoredActivityUi(
+    key = this.key,
     accessibility = this.accessibility,
     activity = this.activity,
     participants = this.participants,
     price = this.price,
-    type = this.type
+    type = this.type,
+    status = null,
+    timeSpent = null
 )
+
+fun BoredActivityUi.toEntityActivity() =
+    BoredActivityEntity(
+        accessibility = this.accessibility,
+        activity = this.activity,
+        participants = this.participants,
+        price = this.price,
+        type = this.type,
+        status = this.status ?: ActivityStatus.EMANDAMENTO,
+        key = this.key,
+        link = this.link,
+        timeSpent = this.timeSpent ?: 0L
+    )
 
